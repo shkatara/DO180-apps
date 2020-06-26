c508bd4ca2f0033a0fa2bafea671589fe9ef1ca0
Shubham <skatara@redhat.com>
2020-06-26 13:07:13 +0100

    scond

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,6 +1,2 @@
 <?php
-print "Hello, World! This is the working version of the appication. It Works with everyyything. \n";
-print "Hello, World! This is the working version of the appication. It Works with everyyything. \n";
-print "Hello, World! This is the working version of the appication. It Works with everyyything. \n";
-print "Hello, World! This is the working version of the appication. It Works with everyyything. \n";
-?>
+print "Hello, World! This is the working version of the appication. It Works with everyyything.
